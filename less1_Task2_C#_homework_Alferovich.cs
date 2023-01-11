bce65f7626acd89b3bea2b7ca5c80835c4494154
AlexanderA <alexander.alferovich@gmail.com>
2023-01-11 13:12:33 +0400

    Final commit

---
+++ less1_Task2_C#_homework_Alferovich.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Пожалуйста, введите первое число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Пожалуйста, введите второе число:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
+{
+Console.WriteLine("Первое число больше второго и является максимальным из этих двух.");
+}
+if (num1 < num2)
+{
+Console.WriteLine("Второе число больше первого и является максимальным из этих двух.");
+}
+if (num1 == num2)
+{
+Console.WriteLine("Числа равны.");
+}
